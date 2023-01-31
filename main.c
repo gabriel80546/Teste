c6cea7c101209319b5d6bfa9b8ac32c665ffca9a
Gabriel Passos <gabrieldudu@protonmail.com>
2023-01-30 21:37:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include "header.h"
 
 int main() {
-  int i;
+  int i, c, h;
   char* tabuleiro;
   tabuleiro = inicializar(tabuleiro);
-  tabuleiro[4] = 'O';
-  mostrarTabuleiro(tabuleiro);
+  for(i = 0; i < 4; i++) {
+    c = rand() % 9 + 0;
+    printf("computador = %i\n", c);
+    printf("Onde você quer jogar? R: ");
+    scanf("%d", &h);
+    tabuleiro[c] = 'O';
+    tabuleiro[h] = 'X';
+    mostrarTabuleiro(tabuleiro);
+  }
   return 0;
 }
